5e6a0a6368ebf31785f95f39821a2a165fa18f10
joo01029 <joo01029@naver.com>
2020-08-13 17:26:33 +0900

    cssanimation (practice)

---
+++ sample/main.js
@@ -0,0 +1,8 @@
+function dodisplay(){
+    var con = document.getElementById("hidden");
+        con.style.display = 'block';
+    }
+function outdisplay(){
+    var con = document.getElementById("hidden");
+        con.style.display = 'none';
+    }
